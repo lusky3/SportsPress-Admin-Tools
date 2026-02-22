9373c2519fcf998e2df5746b6ed83f181b406f4e
lusky3 <lusky3@users.noreply.github.com>
2026-02-22 10:08:03 -0500

    feat(etransfer): update cloudflare worker assets and helper classes

--- sportspress-etransfer-automation/includes/class-admin.php
+++ sportspress-etransfer-automation/includes/class-admin.php
@@ -9,61 +9,67 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-class SPET_Admin {
-    
-    public function __construct() {
+class SPET_Admin
+{
+
+    public function __construct()
+    {
         add_action('spat_admin_init_settings', array($this, 'register_settings'));
         add_action('spat_admin_page_tabs', array($this, 'add_admin_tab'));
         add_action('spat_admin_page_content', array($this, 'add_admin_content'));
     }
-    
-    public function add_admin_tab() {
+
+    public function add_admin_tab()
+    {
         echo '<a href="#etransfer" class="nav-tab">e-Transfer</a>';
     }
-    
-    public function add_admin_content() {
+
+    public function add_admin_content()
+    {
         echo '<div id="etransfer" class="tab-content" style="display:none;">';
         $this->admin_page_content();
         echo '</div>';
     }
-    
-    public function register_settings() {
+
+    public function register_settings()
+    {
         register_setting('spet_settings', 'spet_webhook_secret');
         register_setting('spet_settings', 'spet_service_provider');
         register_setting('spet_settings', 'spet_equivalent_names');
     }
-    
-    public function admin_page_content() {
+
+    public function admin_page_content()
+    {
         if (isset($_POST['save_settings'])) {
             check_admin_referer('spet_save_settings');
-            
+
             if (!current_user_can('manage_woocommerce')) {
                 wp_die(__('You do not have permission to access this page.', 'sportspress-etransfer-automation'));
             }
-            
+
             update_option('spet_webhook_secret', sanitize_text_field($_POST['spet_webhook_secret']));
             update_option('spet_service_provider', sanitize_text_field($_POST['spet_service_provider']));
-            
+
             // Validate and sanitize equivalent names
             $equivalent_names = $this->validate_equivalent_names($_POST['spet_equivalent_names']);
             update_option('spet_equivalent_names', $equivalent_names);
-            
+
             SPET_Name_Matcher::clear_cache();
             echo '<div class="notice notice-success"><p>' . __('Settings saved.', 'sportspress-etransfer-automation') . '</p></div>';
         }
-        
+
         $webhook_secret = get_option('spet_webhook_secret', wp_generate_password(32, false));
         $service_provider = get_option('spet_service_provider', 'generic');
         $equivalent_names = get_option('spet_equivalent_names', $this->get_default_equivalent_names());
-        
+
         if (empty(get_option('spet_webhook_secret'))) {
             update_option('spet_webhook_secret', $webhook_secret);
         }
-        
+
         if (empty(get_option('spet_equivalent_names'))) {
             update_option('spet_equivalent_names', $equivalent_names);
         }
-        ?>
+?>
             <form method="post">
                 <input type="hidden" name="current_tab" value="etransfer">
                 <?php wp_nonce_field('spet_save_settings'); ?>
@@ -110,15 +116,16 @@ class SPET_Admin {
             <?php $this->display_webhook_logs(); ?>
         <?php
     }
-    
-    private function display_webhook_logs() {
-        $logs = SPAT_Database::get_etransfer_logs(50);
-        
+
+    private function display_webhook_logs()
+    {
+        $logs = SPET_Database::get_etransfer_logs(50);
+
         if (empty($logs)) {
             echo '<p>' . __('No webhook activity yet.', 'sportspress-etransfer-automation') . '</p>';
             return;
         }
-        
+
         echo '<table class="wp-list-table widefat fixed striped">';
         echo '<thead><tr>';
         echo '<th>' . __('Timestamp', 'sportspress-etransfer-automation') . '</th>';
@@ -128,7 +135,7 @@ class SPET_Admin {
         echo '<th>' . __('Order', 'sportspress-etransfer-automation') . '</th>';
         echo '<th>' . __('Result', 'sportspress-etransfer-automation') . '</th>';
         echo '</tr></thead><tbody>';
-        
+
         foreach ($logs as $log) {
             echo '<tr>';
             echo '<td>' . esc_html($log->timestamp) . '</td>';
@@ -139,32 +146,34 @@ class SPET_Admin {
             echo '<td>' . esc_html($log->result) . '</td>';
             echo '</tr>';
         }
-        
+
         echo '</tbody></table>';
     }
-    
-    private function get_default_equivalent_names() {
+
+    private function get_default_equivalent_names()
+    {
         return "# Common name equivalencies - one per line\n# Format: FullName|Nickname|Nickname2\nNicholas|Nick\nRichard|Rich|Rick|Dick\nRobert|Rob|Bob|Bobby\nWilliam|Will|Bill|Billy\nJames|Jim|Jimmy\nMichael|Mike|Mikey\nDavid|Dave|Davey\nJoseph|Joe|Joey\nThomas|Tom|Tommy\nChristopher|Chris\nMatthew|Matt\nAnthony|Tony\nDaniel|Dan|Danny\nSteven|Steve|Stephen\nAndrew|Andy|Drew\nJoshua|Josh\nKenneth|Ken|Kenny\nTimothy|Tim|Timmy\nJonathan|Jon|Johnny\nAlexander|Alex|Al\nBenjamin|Ben|Benny\nZachary|Zach|Zack\nSamuel|Sam|Sammy\nPatrick|Pat|Patty\nJeffrey|Jeff\nGregory|Greg\nEdward|Ed|Eddie|Ted\nRonald|Ron|Ronnie\nDonald|Don|Donnie\nCharles|Charlie|Chuck\nElizabeth|Liz|Beth|Betty\nJennifer|Jen|Jenny\nJessica|Jess|Jessie\nSusan|Sue|Susie\nMargaret|Maggie|Meg|Peggy\nDorothy|Dot|Dottie\nDeborah|Deb|Debbie\nKatherine|Kate|Kathy|Katie\nRebecca|Becky|Becca\nPatricia|Pat|Patty|Tricia\nChristine|Chris|Christie\nSamantha|Sam|Sammy\nKimberly|Kim\nMelissa|Mel|Missy\nMichelle|Shelly\nStephanie|Steph\nAmanda|Mandy\nCatherine|Cathy|Cat\nNicole|Nikki|Nicki\nVictoria|Vicky|Tori\nAlexandra|Alex|Alexa";
     }
-    
-    private function validate_equivalent_names($input) {
+
+    private function validate_equivalent_names($input)
+    {
         $input = sanitize_textarea_field($input);
         $lines = explode("\n", $input);
         $validated_lines = array();
-        
+
         foreach ($lines as $line) {
             $line = trim($line);
-            
+
             // Keep empty lines and comments
             if (empty($line) || strpos($line, '#') === 0) {
                 $validated_lines[] = $line;
                 continue;
             }
-            
+
             // Validate the line format
             $names = explode('|', $line);
             $valid_names = array();
-            
+
             foreach ($names as $name) {
                 $name = trim($name);
                 // Only allow letters, spaces, hyphens, apostrophes
@@ -172,13 +181,13 @@ class SPET_Admin {
                     $valid_names[] = $name;
                 }
             }
-            
+
             // Only keep lines with at least 2 valid names
             if (count($valid_names) >= 2) {
                 $validated_lines[] = implode('|', $valid_names);
             }
         }
-        
+
         return implode("\n", $validated_lines);
     }
 }
--- sportspress-etransfer-automation/sportspress-etransfer-automation.php
+++ sportspress-etransfer-automation/sportspress-etransfer-automation.php
@@ -20,25 +20,32 @@ define('SPET_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('SPET_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('SPET_VERSION', '1.0.0');
 
-class SportsPress_ETransfer_Automation {
-    
-    public function __construct() {
+class SportsPress_ETransfer_Automation
+{
+
+    public function __construct()
+    {
         register_activation_hook(__FILE__, array($this, 'check_activation_requirements'));
         add_action('plugins_loaded', array($this, 'init'));
     }
-    
-    public function check_activation_requirements() {
+
+    public function check_activation_requirements()
+    {
         if (!class_exists('SPAT_Plugin_Manager')) {
             deactivate_plugins(plugin_basename(__FILE__));
             wp_die(__('SportsPress e-Transfer Automation requires SportsPress Admin Tools to be installed and activated first.', 'sportspress-etransfer-automation'));
         }
+
+        require_once SPET_PLUGIN_PATH . 'includes/class-database.php';
+        SPET_Database::create_tables();
     }
-    
-    public function init() {
+
+    public function init()
+    {
         if (!$this->check_parent_plugin()) {
             return;
         }
-        
+
         // Register with parent plugin
         SPAT_Plugin_Manager::register_plugin('etransfer_automation', array(
             'name' => 'e-Transfer Automation',
@@ -47,44 +54,49 @@ class SportsPress_ETransfer_Automation {
             'version' => '1.0.0',
             'file' => __FILE__
         ));
-        
+
         // Load functionality if parent module is enabled
         $enabled_modules = get_option('spat_enabled_modules', array());
         if (in_array('etransfer_automation', $enabled_modules)) {
             $this->load_functionality();
         }
     }
-    
-    private function load_functionality() {
+
+    private function load_functionality()
+    {
+        require_once SPET_PLUGIN_PATH . 'includes/class-database.php';
         require_once SPET_PLUGIN_PATH . 'includes/class-name-matcher.php';
         require_once SPET_PLUGIN_PATH . 'includes/class-etransfer-automation.php';
         require_once SPET_PLUGIN_PATH . 'includes/class-admin.php';
         require_once SPET_PLUGIN_PATH . 'includes/class-etransfer-admin.php';
         require_once SPET_PLUGIN_PATH . 'includes/class-file-downloads.php';
-        
+
         new SPET_ETransfer_Automation();
-        new SPET_File_Downloads();
-        
+        new SPETFileDownloads();
+
         if (is_admin()) {
             new SPET_Admin();
             new SPET_ETransfer_Admin();
         }
     }
-    
-    private function check_parent_plugin() {
+
+    private function check_parent_plugin()
+    {
         if (!class_exists('SPAT_Plugin_Manager')) {
             add_action('admin_notices', array($this, 'parent_plugin_missing_notice'));
             return false;
         }
         return true;
     }
-    
-    public function parent_plugin_missing_notice() {
+
+    public function parent_plugin_missing_notice()
+    {
         echo '<div class="notice notice-error"><p>';
         echo __('SportsPress e-Transfer Automation requires SportsPress Admin Tools to be installed and activated.', 'sportspress-etransfer-automation');
         echo '</p></div>';
     }
-    
+
+
 
 }
 
