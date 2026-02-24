8165a32bd8897cab519ea2b3bdc45702f724fb4f
lusky3 <lusky3@users.noreply.github.com>
2026-02-24 02:44:16 -0500

    fix: replace require with require_once in test_headers.php (S2003)

--- test_headers.php
+++ test_headers.php
@@ -1,5 +1,5 @@
 <?php
-require 'C:\development\git\SportsPress-Admin-Tools\tests\bootstrap.php';
+require_once 'C:\development\git\SportsPress-Admin-Tools\tests\bootstrap.php';
 $req = new WP_REST_Request();
 $req->set_header('X-Signature', 'abc');
 var_dump(array_keys($req->get_headers()));
